e76d8cefa3079914408c3fc31b425d4ac8b85e7e
Dima Spivak <dima@spivak.ch>
2017-10-04 22:08:57 -0700

    Initial commit

---
+++ README.rst
@@ -0,0 +1,67 @@
+============================
+HDP topology for clusterdock
+============================
+
+This repository houses the **HDP** topology for `clusterdock`_.
+
+.. _clusterdock: https://github.com/clusterdock/clusterdock
+
+Usage
+=====
+
+Assuming you've already installed **clusterdock** (if not, go `read the docs`_),
+you use this topology by cloning it to a local folder and then running commands
+with the ``clusterdock`` script:
+
+.. _read the docs: http://clusterdock.readthedocs.io/en/latest/
+
+.. code-block:: console
+
+    $ git clone https://github.com/clusterdock/topology_hdp.git
+    $ clusterdock start topology_hdp
+
+To see full usage instructions for the ``start`` action, use ``-h``/``--help``:
+
+.. code-block:: console
+
+    $ clusterdock topology_hdp start -h
+    usage: clusterdock start [--always-pull] [--namespace ns] [--network nw]
+                             [-o sys] [-r url] [-h] [--hdp-version ver]
+                             [--ambari-version ver] [--predictable]
+                             [--dont-start-cluster]
+                             [--primary-node node [node ...]]
+                             [--secondary-nodes node [node ...]]
+                             topology
+    
+    Start a HDP cluster
+    
+    positional arguments:
+      topology              A clusterdock topology directory
+    
+    optional arguments:
+      --always-pull         Pull latest images, even if they're available locally
+                            (default: False)
+      --namespace ns        Namespace to use when looking for images (default:
+                            None)
+      --network nw          Docker network to use (default: cluster)
+      -o sys, --operating-system sys
+                            Operating system to use for cluster nodes (default:
+                            None)
+      -r url, --registry url
+                            Docker Registry from which to pull images (default:
+                            docker.io)
+      -h, --help            show this help message and exit
+    
+    HDP arguments:
+      --hdp-version ver     HDP version to use (default: 2.6.2)
+      --ambari-version ver  Ambari version to use (default: 2.5.2.0)
+      --predictable         If specified, attempt to expose container ports to the
+                            same port number on the host (default: False)
+      --dont-start-cluster  Don't start clusters in Apache Ambari (default: False)
+    
+    Node groups:
+      --primary-node node [node ...]
+                            Nodes of the primary-node group (default: ['node-1'])
+      --secondary-nodes node [node ...]
+                            Nodes of the secondary-nodes group (default:
+                            ['node-2'])
---
+++ start.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import logging
+import re
+import socket
+import textwrap
+
+from ambariclient.client import Ambari
+
+from clusterdock.models import Cluster, client, Node
+from clusterdock.utils import nested_get, wait_for_condition
+
+logger = logging.getLogger('clusterdock.{}'.format(__name__))
+
+DEFAULT_NAMESPACE = 'clusterdock'
+
+AMBARI_AGENT_CONFIG_FILE_PATH = '/etc/ambari-agent/conf/ambari-agent.ini'
+AMBARI_PORT = 8080
+
+
+def main(args):
+    image_prefix = '{}/{}/topology_hdp:hdp{}_ambari{}'.format(args.registry,
+                                                              args.namespace or DEFAULT_NAMESPACE,
+                                                              args.hdp_version,
+                                                              args.ambari_version)
+    primary_node_image = '{}_{}'.format(image_prefix, 'primary-node')
+    secondary_node_image = '{}_{}'.format(image_prefix, 'secondary-node')
+
+    primary_node = Node(hostname=args.primary_node[0], group='primary',
+                        image=primary_node_image, ports=[{AMBARI_PORT: AMBARI_PORT}
+                                                         if args.predictable
+                                                         else AMBARI_PORT])
+
+    secondary_nodes = [Node(hostname=hostname, group='secondary', image=secondary_node_image)
+                       for hostname in args.secondary_nodes]
+
+    cluster = Cluster(primary_node, *secondary_nodes)
+    cluster.primary_node = primary_node
+    cluster.secondary_nodes = secondary_nodes
+    cluster.start(args.network)
+
+    logger.debug('Starting PostgreSQL for Ambari server ...')
+    primary_node.execute('service postgresql start', quiet=not args.verbose)
+    _update_node_names(cluster, quiet=not args.verbose)
+
+    # The HDP topology uses two pre-built images ('primary' and 'secondary'). If a cluster
+    # larger than 2 nodes is started, some modifications need to be done.
+    if len(secondary_nodes) > 1:
+        _remove_files(nodes=secondary_nodes[1:],
+                      files=['/hadoop/hdfs/current/*'])
+
+    # logger.debug('Waiting for PostgreSQL to be started ...')
+    # def condition(node):
+    #     return node.execute('service postgresql status') == 0
+    # def success(time):
+    #     logger.debug('PostgreSQL became started after %s seconds.', time)
+    # def failure(timeout):
+    #     raise TimeoutError('Timed out after {} seconds waiting for '
+    #                        'PostgreSQL to become started.'.format(timeout))
+    # wait_for_condition(condition=condition, condition_args=[primary_node],
+    #                    success=success, failure=failure)
+
+    logger.info('Starting Ambari server ...')
+    primary_node.execute('ambari-server start', quiet=not args.verbose)
+
+    # Docker for Mac exposes ports that can be accessed only with ``localhost:<port>`` so
+    # use that instead of the hostname if the host name is ``moby``.
+    hostname = 'localhost' if client.info().get('Name') == 'moby' else socket.gethostname()
+    port = cluster.primary_node.host_ports.get(AMBARI_PORT)
+    server_url = 'http://{}:{}'.format(hostname, port)
+    logger.info('Ambari server is now reachable at %s', server_url)
+
+    logger.info('Starting Ambari agents ...')
+    for node in cluster:
+        logger.debug('Starting Ambari agent on %s ...', node.fqdn)
+        node.execute('ambari-agent start', quiet=not args.verbose)
+
+    ambari = Ambari(server_url, username='admin', password='admin')
+    for node in secondary_nodes[1:]:
+        logger.debug('Adding %s to cluster ...', node.fqdn)
+        ambari.clusters('cluster').hosts.create(node.fqdn)
+        for component in ambari.clusters('cluster').hosts(secondary_nodes[0].fqdn).components:
+            logger.debug('Adding component (%s) to cluster on host (%s) ...',
+                         component.component_name,
+                         node.fqdn)
+            host_components = ambari.clusters('cluster').hosts(node.fqdn).components
+            host_components.create(component.component_name).wait()
+
+        logger.debug('Installing all registered components on host (%s) ...',
+                     node.fqdn)
+        ambari.clusters('cluster').hosts(node.fqdn).components.install().wait()
+
+    if not args.dont_start_cluster:
+        ambari.clusters('cluster').services.start().wait()
+
+
+def _update_node_names(cluster, quiet):
+    logger.info('Stopping Ambari server ...')
+    command = cluster.primary_node.execute('ambari-server stop', quiet=quiet)
+    if command.exit_code != 0:
+        raise Exception('Ambari server returned non-zero exit code ({}) while '
+                        'stopping. Full output:'
+                        '\n{}'.format(command.exit_code,
+                                      textwrap.indent(command.output,
+                                                      prefix='    ')))
+
+    logger.info('Stopping Ambari agents ...')
+    commands = cluster.execute('ambari-agent stop', quiet=quiet)
+    for node, command in commands.items():
+        if command.exit_code != 0:
+            raise Exception('Ambari agent on node ({}) returned non-zero exit code ({}) while '
+                            'stopping. Full output:'
+                            '\n{}'.format(node, command.exit_code,
+                                          textwrap.indent(command.output, prefix='    ')))
+
+    logger.debug('Creating host name changes JSON ...')
+    host_name_changes = {'cluster': {'node-1.cluster': cluster.primary_node.fqdn,
+                                     'node-2.cluster': cluster.secondary_nodes[0].fqdn}}
+    cluster.primary_node.put_file('/root/host_name_changes.json', json.dumps(host_name_changes))
+    command = ("/usr/jdk64/jdk1.8.0_112/bin/java -cp "
+               "'/etc/ambari-server/conf:/usr/lib/ambari-server/*:"
+               "/usr/share/java/postgresql-jdbc.jar' "
+               "org.apache.ambari.server.update.HostUpdateHelper "
+               "/root/host_name_changes.json > /var/log/ambari-server/ambari-server.out 2>&1")
+    cluster.primary_node.execute(command)
+
+    for node in cluster:
+        ambari_agent_config = node.get_file(AMBARI_AGENT_CONFIG_FILE_PATH)
+
+        logger.debug('Changing server hostname to %s ...', cluster.primary_node.fqdn)
+        # ConfigObj.write returns a list of strings.
+        node.put_file(AMBARI_AGENT_CONFIG_FILE_PATH,
+                      re.sub(r'(hostname)=.*',
+                             r'\1={}'.format(cluster.primary_node.fqdn),
+                             ambari_agent_config))
+
+
+def _remove_files(nodes, files):
+    command = 'rm -rf {}'.format(' '.join(files))
+    logger.info('Removing files (%s) from nodes (%s) ...',
+                ', '.join(files),
+                ', '.join(node.fqdn for node in nodes))
+    for node in nodes:
+        node.execute(command=command)
